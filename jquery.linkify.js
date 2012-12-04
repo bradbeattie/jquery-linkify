fa7d2ca9aa0955766f12428e203675d8fc1df5e6
Brad Beattie <bradbeattie@gmail.com>
2012-12-04 10:51:53 -0800

    Better nested linkification

--- jquery.linkify.js
+++ jquery.linkify.js
@@ -1,46 +1,31 @@
-function linkify(string, buildHashtagUrl, includeW3, target) {
-  string = string.replace(/((http|https|ftp)\:\/\/|\bw{3}\.)[a-z0-9\-\.]+\.[a-z]{2,3}(:[a-z0-9]*)?\/?([a-z0-9\-\._\?\,\'\/\\\+&amp;%\$#\=~])*/gi, function(captured) {
-    var uri;
-    if (captured.toLowerCase().indexOf("www.") == 0) {
-      if (!includeW3) {
-        return captured;
-      }
-      uri = "http://" + captured;
-    } else {
-      uri = captured;
-    }
-    return "<a href=\"" + uri+ "\" target=\"" + target + "\">" + captured + "</a>";;
-  });
-  
-  if (buildHashtagUrl) {
-    string = string.replace(/\B#(\w+)/g, "<a href=" + buildHashtagUrl("$1") +" target=\"" + target + "\">#$1</a>");
-  }
-  return string;
+function replaceURLWithHTMLLinks(text) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    return text.replace(exp,"<a href='$1'>$1</a>");
+}
+
+function recursiveLinkify(element) {
+    var response = $.map(
+        element.contents(),
+        function(element, i) {
+            if (element.nodeType == document.TEXT_NODE) {
+                return replaceURLWithHTMLLinks(element.textContent);
+            }   
+            element = $(element)
+            if (element.prop("tagName") != "A") {
+                return recursiveLinkify(element);
+            } else {
+                return element[0].outerHTML;
+            }   
+        }   
+    );  
+    return response.join("");
 }
 
 (function($) {
-  $.fn.linkify = function(opts) {
-    return this.each(function() {
-      var $this = $(this);
-      var buildHashtagUrl;
-      var includeW3 = true;
-      var target = '_self';
-      if (opts) {
-        if (typeof opts  == "function") {
-          buildHashtagUrl = opts;
-        } else {
-          if (typeof opts.hashtagUrlBuilder == "function") {
-            buildHashtagUrl = opts.hashtagUrlBuilder;
-          }
-          if (typeof opts.includeW3 == "boolean") {
-            includeW3 = opts.includeW3;
-          }
-          if (typeof opts.target == "string") {
-            target = opts.target;
-          }
-        }
-      }
-      $this.html(linkify($this.html(), buildHashtagUrl, includeW3, target));
-    });
-  }
+    $.fn.linkify = function(opts) {
+        return this.each(function() {
+            var element = $(this);
+            element.html(recursiveLinkify(element));
+        }); 
+    }   
 })(jQuery);
